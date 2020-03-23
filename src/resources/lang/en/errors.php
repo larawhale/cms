1d066e499f0b8f4be7834b44ee74c3cfeac0792f
thomas-veen <t.v.d.veen@outlook.com>
2020-03-23 21:44:17 +0100

    More http error translations

--- src/resources/lang/en/errors.php
+++ src/resources/lang/en/errors.php
@@ -2,8 +2,12 @@
 
 return [
     'http' => [
-        '403' => 'Forbidden!',
-        '404' => 'Page not found.',
-        '500' => 'Whoops! Something went wrong.',
+        '401' => 'Unauthorized',
+        '403' => 'Forbidden',
+        '404' => 'Not found',
+        '419' => 'Page expired',
+        '429' => 'Too many requests',
+        '500' => 'Server error',
+        '503' => 'Service unavailable',
     ],
 ];

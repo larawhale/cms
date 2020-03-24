77006db8ae7fe90d5b8bb8662df610a94e2e06ae
thomas-veen <t.v.d.veen@outlook.com>
2020-03-24 19:17:31 +0100

    Update styleguide pagination

--- src/resources/views/styleguide/pagination.blade.php
+++ src/resources/views/styleguide/pagination.blade.php
@@ -3,27 +3,17 @@
         Pagination
     @endslot
 
-    <nav>
-        <ul class="pagination">
-            <li class="page-item disabled">
-                <a class="page-link" href="#">
-                    <i class="fa fa-chevron-left"></i>
-                </a>
-            </li>
+    @php
+        $paginator = new Illuminate\Pagination\LengthAwarePaginator(
+            range(1, 50),
+            50,
+            1,
+            null,
+            [
+                'path' => Route::current()->uri,
+            ],
+        );
+    @endphp
 
-            @foreach (range(0, 4) as $page)
-                <li class="page-item {{ $page == 0 ? 'active' : '' }}">
-                    <a class="page-link" href="#">
-                        {{ $page + 1 }}
-                    </a>
-                </li>
-            @endforeach
-
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                    <i class="fa fa-chevron-right"></i>
-                </a>
-            </li>
-        </ul>
-    </nav>
+    {{ $paginator->links() }}
 @endcomponent

a58dd7b0046006b0c2dd80d0e3031cf71f7f8323
thomas-veen <t.v.d.veen@outlook.com>
2020-03-24 20:14:45 +0100

    Stylelint config

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'stylelint-config-twbs-bootstrap/scss',
+    rules: {
+        'at-rule-no-unknown': null,
+        'declaration-colon-space-after': 'always-single-line',
+        'declaration-colon-space-before': 'never',
+        indentation: 4,
+        'string-quotes': 'single',
+        'value-list-comma-newline-after': 'always',
+        'value-list-comma-space-after': 'always-single-line',
+    },
+};

4a6c5992716cb11a09f2682930791b3ee0c498bd
thomas-veen <t.v.d.veen@outlook.com>
2020-03-24 20:39:29 +0100

    Working stylelint

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,17 @@
 let mix = require('laravel-mix');
+let Stylelint = require('stylelint-webpack-plugin');
 
-mix.js('src/resources/js/app.js', 'src/public/js')
-    .sass('src/resources/sass/app.scss', 'src/public/css');
+mix.extend('stylelint', function(webpackConfig, ...args) {
+    webpackConfig.plugins.push(
+        new Stylelint({
+            configFile: '.stylelintrc.js',
+            context: './src/resources/sass/',
+            fix: true,
+        }),
+    );
+});
+
+mix.stylelint();
+
+// mix.js('src/resources/js/app.js', 'src/public/js')
+//     .sass('src/resources/sass/app.scss', 'src/public/css');

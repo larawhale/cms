2ba8a381e09352e74710ae2a471fb1bc90d259b3
thomas-veen <t.v.d.veen@outlook.com>
2020-06-04 22:00:21 +0200

    Slimmer code with comments

--- src/app/Http/Requests/Concerns/CopiesDotKeys.php
+++ src/app/Http/Requests/Concerns/CopiesDotKeys.php
@@ -2,7 +2,6 @@
 
 namespace LaraWhale\Cms\Http\Requests\Concerns;
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Validator;
 
@@ -25,24 +24,22 @@ trait CopiesDotKeys
         $validator->after(function ($validator) {
             $messages = $validator->messages()->toArray();
 
-            $keys = array_keys($messages);
-
-            $dotKeys = Arr::where($keys, fn ($k) => Str::contains($k, '.'));
-
-            $extraMessages = collect($dotKeys)
+            $extraMessages = collect(array_keys($messages))
+                // Only include keys that are dot notated.
+                ->filter(fn ($k) => Str::contains($k, '.'))
+                // Map those keys from "parent.child" to "parent[child]" and
+                // copy the messages.
                 ->mapWithKeys(function ($k) use ($messages) {
                     $exploded = explode('.', $k);
 
-                    $squareBrackets = array_map(function ($k, $i) {
+                    $squareBrackets = array_map(
                         // Do not apply the square brackets to the first part
                         // of the key. It would otherwise result in
                         // "[parent][child]" which does not make sense at all.
-                        if ($i === 0) {
-                            return $k;
-                        }
-
-                        return "[$k]";
-                    }, $exploded, array_keys($exploded));
+                        fn ($k, $i) => $i === 0 ? $k : "[$k]",
+                        $exploded,
+                        array_keys($exploded),
+                    );
 
                     return [implode($squareBrackets) => $messages[$k]];
                 })->all();

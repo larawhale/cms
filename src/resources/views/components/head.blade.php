ff72d658e936b3e25cf0eaccb95a5d8815762db9
Thomas van der Veen <thomas11juni@hotmail.com>
2020-10-12 20:55:20 +0200

    Use cdn instead of node_modules for font awesome. Fixes #91

---
+++ src/resources/views/components/head.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="{{ asset('vendor/cms/css/app.css') }}">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -1,11 +1,6 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <link rel="stylesheet" href="{{ asset('vendor/cms/css/app.css') }}">
-    </head>
+    @include('cms::components.head')
 
     <body>
         <div id="cms-app">
